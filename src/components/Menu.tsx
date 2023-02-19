a7ad45da541f866024c72ebb7c8f782e7484b4d5
Yulya <iuliiaobr.io@gmail.com>
2023-02-19 16:09:55 +0700

    Adds primary colors and animation to nav menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,6 +12,8 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Logo from './Logo'
+import navItems from '../data/data'
+import MenuNav from './MenuNav'
 
 const solutions = [
   {
@@ -57,7 +59,7 @@ const resources = [
   { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#' },
 ]
 
-function classNames(...classes: []) {
+function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 
@@ -67,7 +69,7 @@ function Menu() {
     <Logo/>
     
     <Popover className="relative bg-white">
-      <div className="flex items-center justify-between p-6 md:justify-start md:space-x-10">
+      <div className="flex items-center justify-between p-6 md:justify-center md:space-x-10">
        
         <div className="-my-2 -mr-2 md:hidden">
           <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -81,14 +83,14 @@ function Menu() {
               <>
                 <Popover.Button
                   className={classNames(
-                    open ? 'text-gray-900' : 'text-gray-500',
-                    'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                    open ? 'text-brown' : 'brown',
+                    'group inline-flex items-center rounded-md bg-white text-brown font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 text-base max-w-sm font-medium text-brown leading-tight'
                   )}
                 >
-                  <span>Solutions</span>
+                  <span className='link link-underline link-underline-brown'>Меню</span>
                   <ChevronDownIcon
                     className={classNames(
-                      open ? 'text-gray-600' : 'text-gray-400',
+                      open ? 'text-brown' : 'text-brown',
                       'ml-2 h-5 w-5 group-hover:text-gray-500'
                     )}
                     aria-hidden="true"
@@ -124,7 +126,7 @@ function Menu() {
                         ))}
                       </div>
                       <div className="bg-gray-50 p-5 sm:p-8">
-                        <a href="#" className="-m-3 flow-root rounded-md p-3 hover:bg-gray-100">
+                        <a href="!#" className="-m-3 flow-root rounded-md p-3 hover:bg-gray-100">
                           <div className="flex items-center">
                             <div className="text-base font-medium text-gray-900">Enterprise</div>
                             <span className="ml-3 inline-flex items-center rounded-full bg-indigo-100 px-3 py-0.5 text-xs font-medium leading-5 text-indigo-800">
@@ -143,73 +145,9 @@ function Menu() {
             )}
           </Popover>
 
-          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-            Pricing
-          </a>
-          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-            Docs
-          </a>
+        <MenuNav data = {navItems}/>
 
-          <Popover className="relative">
-            {({ open }) => (
-              <>
-                <Popover.Button
-                  className={classNames(
-                    open ? 'text-gray-900' : 'text-gray-500',
-                    'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                  )}
-                >
-                  <span>More</span>
-                  <ChevronDownIcon
-                    className={classNames(
-                      open ? 'text-gray-600' : 'text-gray-400',
-                      'ml-2 h-5 w-5 group-hover:text-gray-500'
-                    )}
-                    aria-hidden="true"
-                  />
-                </Popover.Button>
-
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
-                >
-                  <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0">
-                    <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                      <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                        {resources.map((resource) => (
-                          <a
-                            key={resource.name}
-                            href={resource.href}
-                            className="-m-3 block rounded-md p-3 hover:bg-gray-50"
-                          >
-                            <p className="text-base font-medium text-gray-900">{resource.name}</p>
-                            <p className="mt-1 text-sm text-gray-500">{resource.description}</p>
-                          </a>
-                        ))}
-                      </div>
-                    </div>
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
         </Popover.Group>
-        <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-          <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-            Sign in
-          </a>
-          <a
-            href="#"
-            className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-          >
-            Sign up
-          </a>
-        </div>
       </div>
 
       <Transition
@@ -258,15 +196,15 @@ function Menu() {
             </div>
             <div className="py-6 px-5">
               <div className="grid grid-cols-2 gap-4">
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="!#" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Pricing
                 </a>
 
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="!#" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Docs
                 </a>
 
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="!#" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Enterprise
                 </a>
                 {resources.map((resource) => (
@@ -281,14 +219,14 @@ function Menu() {
               </div>
               <div className="mt-6">
                 <a
-                  href="#"
+                  href="!#"
                   className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                 >
                   Sign up
                 </a>
                 <p className="mt-6 text-center text-base font-medium text-gray-500">
                   Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                  <a href="!#" className="text-indigo-600 hover:text-indigo-500">
                     Sign in
                   </a>
                 </p>
---
+++ src/data/data.ts
@@ -0,0 +1,22 @@
+import { MenuItem } from "../types/types";
+
+const navItems: MenuItem[] = [
+  {
+  title: 'Главная',
+  src: '/'
+},
+  {
+  title: 'О нас',
+  src: '/'
+},
+  {
+  title: 'Доставка',
+  src: '/'
+},
+  {
+  title: 'Контакты',
+  src: '/'
+},
+];
+
+export default navItems;
