8c06bc0d1a3e2b289e7aed1b52998e1e61b2bd10
Yulya <iuliiaobr.io@gmail.com>
2023-03-08 11:47:01 +0700

    Add footer component

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,8 +3,10 @@ import BakeryMenu from "../components/bakery-menu/BakeryMenu";
 import { ContactUs } from "../components/contact-us/ContactUs";
 import { DeliveryCards } from "../components/delivery/DeliverCards";
 import FAQ from "../components/faq/FAQ";
+import { Footer } from "../components/footer/Footer";
 import { Hero } from "../components/hero/Hero";
 import Menu from "../components/menu-navigation/Menu";
+import { navItems } from "../data/data";
 
 function Main() {
   return (
@@ -16,6 +18,7 @@ function Main() {
       <Addresses/>
       <FAQ/>
       <ContactUs/>
+      <Footer links={navItems}/>
     </>
   );
 }
