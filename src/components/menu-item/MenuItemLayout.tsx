793b486150e651159e69f28d8f56c4ea001f3d38
Yulya <iuliiaobr.io@gmail.com>
2023-03-16 15:12:24 +0700

    Add product list and display it by id

--- src/components/menu-item/MenuItemLayout.tsx
+++ src/components/menu-item/MenuItemLayout.tsx
@@ -14,14 +14,14 @@
   ```
 */
 import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Menu, Popover, Tab, Transition } from '@headlessui/react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
 import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import Filter from './Filter'
 import MenuList from '../bakery-menu/MenuList'
 import { bakeryMenuData } from '../../data/data'
+import ProductsList from './ProductsList'
 
 
 const sortOptions = [
@@ -31,83 +31,6 @@ const sortOptions = [
   { name: 'Price: Low to High', href: '#' },
   { name: 'Price: High to Low', href: '#' },
 ]
-const filters = [
-  {
-    id: 'category',
-    name: 'Category',
-    options: [
-      { value: 'tees', label: 'Tees' },
-      { value: 'crewnecks', label: 'Crewnecks' },
-      { value: 'hats', label: 'Hats' },
-      { value: 'bundles', label: 'Bundles' },
-      { value: 'carry', label: 'Carry' },
-      { value: 'objects', label: 'Objects' },
-    ],
-  },
-]
-
-const products1 = [
-  {
-    id: 1,
-    name: 'Focus Paper Refill',
-    href: '#',
-    price: '$13',
-    description: '3 sizes available',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-01.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-  },
-  {
-    id: 2,
-    name: 'Focus Card Holder',
-    href: '#',
-    price: '$64',
-    description: 'Walnut',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-02.jpg',
-    imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
-  },
-  {
-    id: 3,
-    name: 'Focus Carry Pouch',
-    href: '#',
-    price: '$32',
-    description: 'Heather Gray',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-03.jpg',
-    imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
-  },
-  // More products...
-]
-const products2 = [
-  {
-    id: 7,
-    name: 'Electric Kettle',
-    href: '#',
-    price: '$149',
-    description: 'Black',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-07.jpg',
-    imageAlt: 'Close up of long kettle spout pouring boiling water into pour-over coffee mug with frothy coffee.',
-  },
-  {
-    id: 8,
-    name: 'Leather Workspace Pad',
-    href: '#',
-    price: '$165',
-    description: 'Black',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-08.jpg',
-    imageAlt:
-      'Extra large black leather workspace pad on desk with computer, wooden shelf, desk organizer, and computer peripherals.',
-  },
-  {
-    id: 9,
-    name: 'Leather Long Wallet',
-    href: '#',
-    price: '$118',
-    description: 'Natural',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-09.jpg',
-    imageAlt:
-      'Leather long wallet held open with hand-stitched card dividers, full-length bill pocket, and simple tab closure.',
-  },
-  // More products...
-]
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
@@ -115,9 +38,12 @@ function classNames(...classes: any) {
 
 export default function MenuItemLayout() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
-
+  const [menu, setMenu] = useState(bakeryMenuData[0])
+  
   const handleClick = (id: string) => {    
-  console.log(id) 
+  return bakeryMenuData.filter((item) => {
+    return item.id === id ? setMenu(item) : null;
+  }) 
   }
 
   return (
@@ -162,51 +88,6 @@ export default function MenuItemLayout() {
                       <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
                   </div>
-
-                  {/* Filters */}
-                  <form className="mt-4">
-                    {filters.map((section) => (
-                      <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
-                        {({ open }) => (
-                          <>
-                            <h3 className="-mx-2 -my-3 flow-root">
-                              <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-sm text-gray-400">
-                                <span className="font-medium text-gray-900">{section.name}</span>
-                                <span className="ml-6 flex items-center">
-                                  <ChevronDownIcon
-                                    className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-5 w-5 transform')}
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              </Disclosure.Button>
-                            </h3>
-                            <Disclosure.Panel className="pt-6">
-                              <div className="space-y-6">
-                                {bakeryMenuData.map((option, optionIdx) => (
-                                  <div key={option.id} className="flex items-center">
-                                    <input
-                                      id={`filter-mobile-${option.id}-${optionIdx}`}
-                                      name={`${option.id}[]`}
-                                      defaultValue={option.title}
-                                      type="checkbox"
-                                      // defaultChecked={option.checked}
-                                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                    />
-                                    <label
-                                      htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                      className="ml-3 text-sm text-gray-500"
-                                    >
-                                      {option.title}
-                                    </label>
-                                  </div>
-                                ))}
-                              </div>
-                            </Disclosure.Panel>
-                          </>
-                        )}
-                      </Disclosure>
-                    ))}
-                  </form>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
@@ -284,51 +165,27 @@ export default function MenuItemLayout() {
                 Products
               </h2>
 
-              <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                {products1.map((product) => (
-                  <a key={product.id} href={product.href} className="group">
+              <ProductsList menu = {menu}/>
+              {/* <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                {menu.menu.map((product) => (
+                  <a key={product.id} href='!#' className="group">
                     <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3">
                       <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
+                        src={product.imgSrc}
+                        alt={product.title}
                         className="h-full w-full object-cover object-center group-hover:opacity-75"
                       />
                     </div>
                     <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                      <h3>{product.name}</h3>
+                      <h3>{product.title}</h3>
                       <p>{product.price}</p>
                     </div>
                     <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
                   </a>
                 ))}
-              </div>
+              </div> */}
             </section>
 
-
-            <section aria-labelledby="more-products-heading" className="mt-16 pb-24">
-              <h2 id="more-products-heading" className="sr-only">
-                More products
-              </h2>
-
-              <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                {products2.map((product) => (
-                  <a key={product.id} href={product.href} className="group">
-                    <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3">
-                      <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
-                        className="h-full w-full object-cover object-center group-hover:opacity-75"
-                      />
-                    </div>
-                    <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                      <h3>{product.name}</h3>
-                      <p>{product.price}</p>
-                    </div>
-                    <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
-                  </a>
-                ))}
-              </div>
-            </section>
           </div>
         </main>
 
---
+++ src/components/menu-item/ProductsList.tsx
@@ -0,0 +1,24 @@
+
+export default function ProductsList({menu}: any) {
+  if(menu === undefined) return null;
+  return(
+    <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+    {menu.menu.map((product: any) => (
+      <a key={product.id} href='!#' className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3">
+          <img
+            src={product.imgSrc}
+            alt={product.title}
+            className="h-full w-full object-cover object-center group-hover:opacity-75"
+          />
+        </div>
+        <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
+          <h3>{product.title}</h3>
+          <p>{product.price}</p>
+        </div>
+        <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
+      </a>
+    ))}
+  </div>
+  )
+}
