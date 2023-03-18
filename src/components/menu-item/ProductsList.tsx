289f09daa77e202dbde0d87d169aa31b4c297259
Yulya <iuliiaobr.io@gmail.com>
2023-03-18 18:00:16 +0700

    Display ingredients on hover

--- src/components/menu-item/ProductsList.tsx
+++ src/components/menu-item/ProductsList.tsx
@@ -1,24 +1,31 @@
+import { BakeryMenuItemTypes } from "../../types/types";
 
 export default function ProductsList({menu}: any) {
   if(menu === undefined) return null;
   return(
-    <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-    {menu.menu.map((product: any) => (
-      <a key={product.id} href='!#' className="group">
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3">
-          <img
-            src={product.imgSrc}
-            alt={product.title}
-            className="h-2/4 object-cover object-center group-hover:opacity-75"
-          />
-        </div>
-        <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-          <h3>{product.title}</h3>
-          <p>{product.price}</p>
-        </div>
-        <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
-      </a>
-    ))}
-  </div>
+    <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {menu.menu.map((item: BakeryMenuItemTypes) => (
+              <div key={item.id} className="group card">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+                  <img
+                    src={item.imgSrc}
+                    alt={item.title}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <div className="flex justify-between items-center">
+                <h3 className="mt-2 text-base font-semibold text-gray-900 ">
+                    {item.title}
+                </h3>
+                <p>{item.price}</p>
+                </div>
+                <p className="my-4 text-gray-500">{item.description}</p>
+
+                <div className="hidden">
+                  <h2 className="hidden-text">{item.ingredient.join(', ')}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
   )
 }
