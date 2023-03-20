00bbafde0974324be125bf72d52bec7b4721e298
Yulya <iuliiaobr.io@gmail.com>
2023-03-20 21:00:17 +0700

    Add styles to selected address

--- src/components/addresses/AddressList.tsx
+++ src/components/addresses/AddressList.tsx
@@ -3,15 +3,17 @@ import { DeliveryDataTypes } from "../../types/types";
 type AddressListProps = {
   data: DeliveryDataTypes[];
   setAddressId: any;
+  addressById: DeliveryDataTypes;
 };
-function AddressList({data, setAddressId}: AddressListProps) {
+function AddressList({data, setAddressId, addressById}: AddressListProps) {
 
 
   return(
     <>
     {data.map((item: DeliveryDataTypes) => {
+      const isSelected = item.id === addressById.id;
       return(
-        <button className="hover:scale-110 transition-all duration-300 hover-1" onClick = {() => setAddressId(item)} key={item.id}>
+        <button className={`hover:scale-110 transition-all duration-300 ${isSelected ? 'selected' : ''}`} onClick = {() => setAddressId(item)} key={item.id}>
     <div>
             <h3 className="border-l border-black pl-6 font-semibold text-gray-900 ">{item.title}</h3>
             <address className="border-l border-gray-200 pt-2 pl-6 not-italic text-gray-600">
