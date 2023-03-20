29623d5d3fb07062381414a21257745f6b777550
Yulya <iuliiaobr.io@gmail.com>
2023-03-20 21:05:09 +0700

    Add hover effect to address item

--- src/components/addresses/AddressList.tsx
+++ src/components/addresses/AddressList.tsx
@@ -13,7 +13,7 @@ function AddressList({data, setAddressId, addressById}: AddressListProps) {
     {data.map((item: DeliveryDataTypes) => {
       const isSelected = item.id === addressById.id;
       return(
-        <button className={`hover:scale-110 transition-all duration-300 ${isSelected ? 'selected' : ''}`} onClick = {() => setAddressId(item)} key={item.id}>
+        <button className={`hover:scale-110 hover-1 transition-all duration-300 ${isSelected ? 'selected' : ''}`} onClick = {() => setAddressId(item)} key={item.id}>
     <div>
             <h3 className="border-l border-black pl-6 font-semibold text-gray-900 ">{item.title}</h3>
             <address className="border-l border-gray-200 pt-2 pl-6 not-italic text-gray-600">
