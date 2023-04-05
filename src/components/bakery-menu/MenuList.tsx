1d6370902363263451f3b1d948db6bea43205ff7
Yulya <iuliiaobr.io@gmail.com>
2023-04-05 14:34:34 +0700

    Highlight chosen menu

--- src/components/bakery-menu/MenuList.tsx
+++ src/components/bakery-menu/MenuList.tsx
@@ -17,11 +17,15 @@ export default function MenuList({handleClick, data, menu, dispatch}: MenuListPr
     <div className="my-2 mx-auto max-w-4xl">
     <ul className="flex flex-wrap justify-center text-lg leading-8">
     {data.map((item) => {
+      let isSelected;
+      if(menu) {
+        isSelected = item.id === menu.id;
+      }
       return(
           <li className="w-max hover:scale-110 hover:-translate-y-2 transition-all duration-500 text-brown" key={item.id}>
             <button 
             onClick={() => {handleClick(item.id)}} 
-            className='text-brown inline-block px-3.5 m-1 hover:text-shadow transition-all '>{item.title}
+            className={`text-brown inline-block px-3.5 m-1 ${isSelected ? 'selected' : ''} hover:text-shadow transition-all `}>{item.title}
           </button></li>
           )
         })}
